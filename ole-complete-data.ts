5c25541a3b6db83ce65d869bfa3aa1ccb045605d
sunshinenj <chenjhua@hotmail.com>
2018-05-25 08:04:41 -0400

    Create ole-complete-data.ts

---
+++ ole-complete-data.ts
@@ -0,0 +1,143 @@
+export interface oledataAem {
+
+  "mainheading": string;
+  "headingPara" : string;
+  "chossenEnroll" : string;
+  "ziptext" : string;
+  "Premiumtext" : string;
+  "whatNeedtoEnrollText" : string;
+  "whatNeedtoEnrolListText" : string;
+  "learnMoreBtn" : string;
+  "EnrollDisclamer" : string;
+  "Nextbtn" : string;
+  "CanclEnrol" : string;
+  "EnrollDisclamerInfo" : string;
+  "NameValidtntxt" : string;
+  "WhatUrGender" : string;
+   "mainPhoneNumberfield": string;
+    "authorizationHeadText":string;
+  "stepHeadText":string;
+  "confirmNumberText":string;
+  "confirmationHeadText":string;
+  "successfullMsg":string;
+  "confirmtnPagelist1":string;
+  "confirmtnPagelist2":string;
+  "confirmtnPagelist3":string;
+  "confirmtnPagelist4":string;
+  "printSummary":string;
+  "nextSteppara":string;
+  "pdpBtn":string;
+  "mspBtn":string;
+  "emailSummaryLinkText":string;
+  "goPaperlessText":string;
+  "confirmEmailText":string;
+  "signUpBtn":string;
+  "paperlessDeliveryDesc":string;
+  'pcpHeaderText': string;
+  'yourProgessText': string;
+  'zeroPergentageText': string;
+  'hundredPercentageText': string;
+  'pcpHelpText1': string;
+  'pcpHelpText2': string;
+  'pcpHelpTextLast': string;
+  'providersNameText': string;
+  'lookupProviderButtonText': string;
+  'pffsPlanText': string;
+  'providerContactHeaderText': string;
+  'providerContactInfText': string;
+  'doctor1NameText': string;
+  'genericPhoneNoText': string;
+  'genericPhoneNoFormatText': string;
+  'genericCityText': string;
+  'genericStateText': string;
+  'genericZIPText': string;
+  'doctor2NameText': string;
+  'hospitalHeaderText': string;
+  'hospitalNameText': string;
+  'nextButtonText': string;
+  'backButtonText': string;
+  'cancelEnrollmentButtonText': string;
+  'PCPTooltipText': string;
+
+  'MedicInsnceInfoHeading': string;
+  'MedicInsnceInfoparaText': string;
+  'MedicInsnceInfoHeadinglegend': string;
+  'Loremipsum': string;
+  'filltheinfotext': string;
+  'FirstName': string;
+  'FirstNameValidtntxt': string;
+  'MiddleInitial': string;
+  'MiddleInitialValidtntxt': string;
+  'LastName': string;
+  'MedicareClaimNumber': string;
+  'MedicareClaimNumvaldtn': string;
+  'HospitlPartAEffectDate': string;
+  'EffectDateHelpertxt': string;
+  'EffectDatevadltntxt': string;
+  'Indicatesrequiredfields': string;
+
+
+  'PersonalInfoHeading': string;
+  'PersonalInfoNamelist': string;
+  'PersonalInfodobText': string;
+  'Whatyourgender': string;
+  'Male': string;
+  'Female': string;
+  'makeSelection': string;
+  'PermanentAddress': string;
+  'StreetAddress': string;
+  'StreetAddressHelprText': string;
+  'StreetAddressValidtn': string;
+  'ApartmentOrSuite': string;
+  'CityValdtn': string;
+  'addrsFeildEndPara': string;
+  'sameAsaddrsFeildPara': string;
+  'Yestxt': string;
+  'Notxt': string;
+  'MailingAddress': string;
+  'stateValdtn': string;
+  'ZipValdtntxt': string;
+  'Edittxt': string;
+  'mainPhoneNumbertxt': string;
+  'PhoneNumberValdtn': string;
+  'Addanothrphonenumbertxt': string;
+  'yourEmailAddrstxt': string;
+  'EmailAddresstxt': string;
+  'EmailAddressHelptxt': string;
+  'EmailAddressValidnTxt': string;
+  'CnfrmEmailAddressParatxt': string;
+  'GopaperlessTxt': string;
+  'checkToAgreetxt': string;
+  'LanguageCntctUnitedTxt': string;
+  'LanguageTxt': string;
+  'EnglishTxt': string;
+  'SpanishTxt': string;
+  'ContactUnitedEnrollment': string;
+
+
+  'ReviewdocumntnsubmtTxt': string;
+  'PlanFTxt': string;
+  'ReviewdocumntnListTxt': string;
+  'PleaseReviewTxtPara': string;
+  'PlanEffectiveDateTxt': string;
+  'SubmitApplicationTxt': string;
+
+
+
+  'CancelPopupHeadrTxt': string;
+  'CancelPopupBodypara': string;
+  'CancelPopupCancelTxt': string;
+  'viewLearnEnrlmntHeadrTxt' : string;
+  'viewLearnEnrlmntBodyPara' : string;
+  'ReturnApplicationTxt': string;
+
+
+   'PrelimnaryQuestinsTxt': string;
+  'PayMonthlyplanPremiumTxt': string;
+  'SpclElectinPeriodTxt': string;
+  'CovrageHlthInfoTxt': string;
+  'AgentBrokerIDTxt': string;
+  'EndStageRenalDiseaseTxt': string;
+  'EndStageRenalDiseaseLegendPara': string;
+
+}
